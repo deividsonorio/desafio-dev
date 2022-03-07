d0c1584495a8b6786e3c3c59e3f0ffd9c2e339b1
Deivid Schlosser Onorio <deivid.onorio@boxdelivery.com.br>
2022-03-07 15:28:05 -0300

    Update cnab view for unit tests

--- app/cnab/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-from rest_framework import routers
-from .views import UploadViewSet
-
-router = routers.DefaultRouter()
-router.register('upload/', UploadViewSet, basename="upload")
--- app/cnab/views.py
+++ app/cnab/views.py
@@ -1,5 +1,7 @@
 import time
 
+from rest_framework import status
+
 from .tasks import handle_cnab_file
 from django.conf import settings
 from rest_framework.response import Response
@@ -20,15 +22,21 @@ class UploadViewSet(ViewSet):
     serializer_class = UploadSerializer
 
     def list(self, request):
-        return Response("GET - Envie arquivos txt no formato CNAB por POST")
+        return Response("GET - Envie arquivos txt no formato CNAB por POST", status=status.HTTP_200_OK)
 
     def create(self, request):
         if request.FILES.get('arquivo_cnab'):
             file_uploaded = request.FILES.get('arquivo_cnab')
-            file_save = handle_uploaded_file(file_uploaded)
-            handle_cnab_file.delay(file_save)
-            response = 'Arquivo enviado para processamento. {}'.format(file_uploaded.name)
+            filename = file_uploaded.name
+            if filename.endswith('.txt'):
+                file_save = handle_uploaded_file(file_uploaded)
+                handle_cnab_file.delay(file_save)
+                response = 'Arquivo enviado para processamento. {}'.format(file_uploaded.name)
 
-            return Response(response)
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                response = 'Envie arquivo com extensão .txt. {}'.format(file_uploaded.name)
 
-        return Response("Envie arquivos txt no formato CNAB por POST")
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response("Nenhum arquivo recebido", status=status.HTTP_400_BAD_REQUEST)
